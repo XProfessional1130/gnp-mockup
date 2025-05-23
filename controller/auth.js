ad7ca5cbfb9e5ff4bfbcc1a2dd0c39ca0b4497e9
Code-Crazier <devnexus.enthusiast@gmail.com>
2025-05-22 20:52:18 -0400

    add: log to fix bug

--- controller/auth.js
+++ controller/auth.js
@@ -6,6 +6,7 @@ dotenv.config();
 module.exports = {
   auth: (req, res) => {
     const token = jwt.sign({ clientId: req.body.client_id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    console.log(token);
     res.status(200).json({
       success: true,
       access_token: token,
